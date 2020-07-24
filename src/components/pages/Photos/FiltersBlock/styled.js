fb4b082a34d038dde60cc5ac80b94d35e9737d8b
Ivan Marakhovskyi <ivan.marakhovskyi@gdxtec.com>
2020-07-24 10:51:28 +0300

    minor style fixes

--- src/components/pages/Photos/FiltersBlock/styled.js
+++ src/components/pages/Photos/FiltersBlock/styled.js
@@ -8,12 +8,17 @@ export const FiltersBlockWrapper = styled.div`
 `
 
 export const StyledFilterButton = styled.button`
-  background-color: ${({ isActive }) => isActive && 'rgb(90, 50, 168)'};
+  outline: none;
+  background-color: ${({ isActive }) => isActive ? 'rgb(90, 50, 168)' : 'white'};
   color: ${({ isActive }) => isActive && 'white'};
   padding: 0.3rem 0.5rem;
   border-radius: 0.2rem;
   font-size: 1rem;
   border: 1px solid rgb(90, 50, 168);
+
+  &:hover, &:active, &:focus {
+    outline: none;
+  }
 
   &:not(:last-child) {
     margin-right: 0.5rem;
