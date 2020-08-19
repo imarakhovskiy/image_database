64c1024319a2000c4a92b09990ce9f21eda37a22
Ivan Marakhovskyi <ivan.marakhovskyi@gdxtec.com>
2020-08-19 15:25:41 +0300

    Updates

---
+++ precache-manifest.8a842fc24119bfd88cbb3ba74680026b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbd281ece75e486f2890d30dc2a98997",
+    "url": "/image_database/index.html"
+  },
+  {
+    "revision": "53521219380e093e3cb9",
+    "url": "/image_database/static/css/main.9e9bb71c.chunk.css"
+  },
+  {
+    "revision": "7ba00d800cef81bab44f",
+    "url": "/image_database/static/js/2.dd9507a8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/image_database/static/js/2.dd9507a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53521219380e093e3cb9",
+    "url": "/image_database/static/js/main.92b9c926.chunk.js"
+  },
+  {
+    "revision": "5ca97195e0f2847d3afe",
+    "url": "/image_database/static/js/runtime-main.22ef2a58.js"
+  },
+  {
+    "revision": "373d34ae4620d853b0e922ef70c03919",
+    "url": "/image_database/static/media/arrow.373d34ae.svg"
+  },
+  {
+    "revision": "8e01c9fe192ead030b9aaa60a9d41cb4",
+    "url": "/image_database/static/media/calendar.8e01c9fe.svg"
+  },
+  {
+    "revision": "aa24918d36e78a1a8fb2ffc7644d7595",
+    "url": "/image_database/static/media/discover.aa24918d.svg"
+  },
+  {
+    "revision": "f8114a63a8a0ebf1db36f4145d75352c",
+    "url": "/image_database/static/media/files.f8114a63.svg"
+  },
+  {
+    "revision": "810274f08ccc5aef3c797e431fe342b2",
+    "url": "/image_database/static/media/link.810274f0.svg"
+  },
+  {
+    "revision": "b90bac84d798465b1f87fee9d491cbe1",
+    "url": "/image_database/static/media/photos.b90bac84.svg"
+  },
+  {
+    "revision": "f582311e8d988d28f0a51efdbdc79163",
+    "url": "/image_database/static/media/share.f582311e.svg"
+  }
+]);
